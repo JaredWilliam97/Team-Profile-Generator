a15e3648c1fb4dc6f9ffbc7871f6a94bd7db3cdc
Jared Sutch <sutch002@gmail.com>
2021-05-18 21:17:32 -0500

    updated html page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -33,5 +33,35 @@ function renderIntern(employee) {
     </div>
   </div>`;
 }
+function renderHeader() {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="./bootstrap.min.css" />
+  
+      <title>Team Profile</title>
+      </head>
+      <body>
+        <header><h1>My Team</h1></header>`;
+}
 
-// module.exports =
+function renderFooter() {
+  return ` </div>
+    </div>
+  </body>
+</html>`;
+}
+
+function generateMarkdown(teamMembers) {
+  return `
+  ${renderHeader()}
+ ${renderManager(teamMembers)}
+  ${renderEngineer(teamMembers)}
+  ${renderIntern(teamMembers)}
+  ${renderFooter()}`;
+}
+
+module.exports = generateMarkdown;
